6e6f44fd640bc4c70ead01f6ffb540de3fbee30a
Sam Fisher <sammee1@gmail.com>
2023-03-23 16:18:28 -0500

    Update Poll.js

--- PollApp/poll_app/src/components/Poll.js
+++ PollApp/poll_app/src/components/Poll.js
@@ -3,7 +3,7 @@ import ProgressBar from '@/components/ProgressBar';
 
 export default function PollingComponent() {
   const [selectedOption, setSelectedOption] = useState("");
-  const [pollResults, setPollResults] = useState({ option1: 0, option2: 0, option3: 0 });
+  const [pollResults, setPollResults] = useState({ bulbasaur: 0, charmander: 0, squirtle: 0 });
 
   const handleOptionChange = (e) => {
     setSelectedOption(e.target.value);
@@ -19,48 +19,48 @@ export default function PollingComponent() {
 
   return (
     <div className="flex flex-col items-start gap-8">
-      <form onSubmit={handlePollSubmit} className="w-full max-w-lg">
+      <form onSubmit={handlePollSubmit} className="w-full">
         <div className="flex flex-col items-center mb-4 gap-3">
-          <div className='text-white text-3xl'>This is a Poll</div>
-          <label htmlFor="option1">
+        <div className='text-white text-3xl text-center'>Which first gen starter is your favorite?</div>
+          <label htmlFor="bulbasaur">
             <input
               type="radio"
-              id="option1"
+              id="bulbasaur"
               name="poll-option"
-              value="option1"
-              checked={selectedOption === "option1"}
+              value="bulbasaur"
+              checked={selectedOption === "bulbasaur"}
               onChange={handleOptionChange}
               className="mr-2"
             />
-            <span className="text-sm text-white">Option 1</span>
+            <span className="text-2xl text-white">Bulbasaur</span>
           </label>
-          <label htmlFor="option2">
+          <label htmlFor="charmander">
             <input
               type="radio"
-              id="option2"
+              id="charmander"
               name="poll-option"
-              value="option2"
-              checked={selectedOption === "option2"}
+              value="charmander"
+              checked={selectedOption === "charmander"}
               onChange={handleOptionChange}
               className="mr-2"
             />
-            <span className="text-sm text-white">Option 2</span>
+            <span className="text-2xl text-white">Charmander</span>
           </label>
-          <label htmlFor="option3">
+          <label htmlFor="squirtle">
             <input
               type="radio"
-              id="option3"
+              id="squirtle"
               name="poll-option"
-              value="option3"
-              checked={selectedOption === "option3"}
+              value="squirtle"
+              checked={selectedOption === "squirtle"}
               onChange={handleOptionChange}
               className="mr-2"
             />
-            <span className="text-sm text-white">Option 3</span>
+            <span className="text-2xl text-white">Squirtle</span>
           </label>
         </div>
         <button
-          disabled = {pollResults.option1 === 100 || pollResults.option2 === 100}
+          disabled = {pollResults.bulbasaur === 100 || pollResults.charmander === 100 || pollResults.squirtle === 100}
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
         >
@@ -68,13 +68,16 @@ export default function PollingComponent() {
         </button>
       </form>
       
-      <div className="bg-gray-100 py-6 pl-8 pr-12 rounded-lg flex flex-col">
+      <div className="bg-gray-100 py-6 pl-8 pr-12 rounded-lg flex flex-col w-full">
         <h2 className="text-lg font-bold mb-2">Results:</h2>
-        <div className="w-96">
-          Option 1: <ProgressBar pollResults={pollResults.option1} />
+        <div className="">
+          Bulbasaur: <ProgressBar pollResults={pollResults.bulbasaur} />
         </div>
         <div>
-          Option 2: <ProgressBar pollResults={pollResults.option2} />
+          Charmander: <ProgressBar pollResults={pollResults.charmander} />
+        </div>
+        <div>
+          Squirtle: <ProgressBar pollResults={pollResults.squirtle} />
         </div>
       </div>
     </div>
