6b75fe6b27e917244afa57324131056fd6f2812b
Sam Fisher <sammee1@gmail.com>
2023-03-24 15:33:48 -0500

    edit alt tag

--- PollApp/poll_app/src/components/ProgressBar.js
+++ PollApp/poll_app/src/components/ProgressBar.js
@@ -7,7 +7,7 @@ export default function ProgressBar(props) {
     <div className="h-8 w-full bg-slate-300 rounded-md m-3">
       <div style={{ width: `${pollResults}%`}} className={`h-full bg-purple-500 rounded-md text-right transition-width duration-1000 ease-in-out`}>
         <span className="text-white pl-3 font-bold inline-flex">
-          <img className="object-scale-down h-10 w-10 pr-2 pb-2" src={`${image}`} alt="" />
+          <img className="object-scale-down h-10 w-10 pr-2 pb-2" src={`${image}`} alt={`${image}`}/>
           {`${pollResults}%`}
         </span>
       </div>
